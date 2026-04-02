339753adf718d71584df567cc3bec97ce6e69db6
subhadeep-webart <subhadeep.das@webart.technology>
2026-04-02 18:25:41 +0530

    coimpleted

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,40 @@
+// import { NextResponse } from "next/server";
+// import { verifyToken } from "./lib/jwt";
+
+// export function middleware(req) {
+//     const token = req.cookies.get("adminToken")?.value;
+//     const { pathname } = req.nextUrl;
+
+//     const isLoginPage = pathname === "/admin/login";
+
+//     if (isLoginPage && token) {
+//         const decoded = verifyToken(token);
+//         console.log("Decoded====>", decoded);
+//         if (decoded) {
+//             return NextResponse.redirect(new URL("/admin/home/banner", req.url));
+//         }
+//     }
+
+//     if (pathname.startsWith("/admin") && !isLoginPage) {
+//         if (!token) {
+//             return NextResponse.redirect(new URL("/admin/login", req.url));
+//         }
+
+//         const decoded = verifyToken(token);
+
+//         if (!decoded || decoded.role !== "admin") {
+//             return NextResponse.redirect(new URL("/admin/login", req.url));
+//         }
+//     }
+
+//     return NextResponse.next();
+// }
+
+// export const config = {
+//     matcher: ["/admin/:path*"],
+// };
+
 import { NextResponse } from "next/server";
-import { verifyToken } from "./lib/jwt";
 
 export function middleware(req) {
     const token = req.cookies.get("adminToken")?.value;
@@ -7,23 +42,19 @@ export function middleware(req) {
 
     const isLoginPage = pathname === "/admin/login";
 
+    // If already logged in → redirect from login page
     if (isLoginPage && token) {
-        const decoded = verifyToken(token);
-        console.log("Decoded====>", decoded);
-        if (decoded) {
-            return NextResponse.redirect(new URL("/admin/home/banner", req.url));
-        }
+        return NextResponse.redirect(
+            new URL("/admin/home/banner", req.url)
+        );
     }
 
+    // Protect admin routes
     if (pathname.startsWith("/admin") && !isLoginPage) {
         if (!token) {
-            return NextResponse.redirect(new URL("/admin/login", req.url));
-        }
-
-        const decoded = verifyToken(token);
-
-        if (!decoded || decoded.role !== "admin") {
-            return NextResponse.redirect(new URL("/admin/login", req.url));
+            return NextResponse.redirect(
+                new URL("/admin/login", req.url)
+            );
         }
     }
 
