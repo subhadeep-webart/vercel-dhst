e95f1831c8c6b58bad6fadcf8b02fa1a3d68f694
subhadeep-webart <subhadeep.das@webart.technology>
2026-04-06 17:25:27 +0530

    Completed

--- src/components/admin/common/BannerForm.jsx
+++ src/components/admin/common/BannerForm.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import InputField from "@/components/ui/admin/input/InputField"
 import TextArea from "@/components/ui/admin/input/Textarea"
 import Label from "@/components/ui/admin/Label"
